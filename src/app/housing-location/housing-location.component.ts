279b7d34ba235e92d51854cb20fc17c6e44ce3a4
atthegallery <joris.schelfaut@gmail.com>
2024-06-26 16:32:22 +0200

    add an input to the housing location component #6

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HousingLocation} from '../housing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
 export class HousingLocationComponent {
-
+  // Input decorator (denoted by @ symbol)
+  // exclamation point (not-null assertion operator) denotes that the property will be passed into the component
+  //@Input() housingLocation!:HouseLocation;
+  @Input() housingLocation!: HousingLocation;
 }
